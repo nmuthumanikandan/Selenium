dc9e7790caa05ba7f3d8964259f791c7a15ad03e
nmuthumanikandan <78196172+nmuthumanikandan@users.noreply.github.com>
2021-03-05 10:45:55 +0530

    Update Redbus.java

--- Redbus.java
+++ Redbus.java
@@ -1,7 +1,11 @@
 package week3.day2;
 
+import java.awt.List;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -57,26 +61,55 @@ public class Redbus {
 		
 		driver.findElement(By.id("onward_cal")).click(); 
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//td[@class='past day']/following-sibling::td[2]")).click();
-		Thread.sleep(2000);
+		//driver.findElement(By.xpath("//td[@class='past day']/following-sibling::td[2]")).click();
 		
-		driver.findElement(By.xpath("//button[@id='search_btn']")).click(); 
+		driver.findElement(By.xpath("//td[@class='current day']")).click(); 
 		
-		Thread.sleep(30000);
+		Thread.sleep(3000);
 		
 		System.out.println("Date selected");
 		
+		Thread.sleep(3000);
 		
-		String bus=driver.findElement(By.xpath("//span[@class='f-bold busFound']")).getText(); 
+		driver.findElement(By.xpath("//button[@id='search_btn']")).click(); 
+		Thread.sleep(20000);
+		
+		
+		/*String bus=driver.findElement(By.xpath("//span[@class='f-bold busFound']")).getText(); 
 		Thread.sleep(2000);
 		
 		System.out.println("No of bus found is: "+ bus);
+		*/
+		
+		String text = driver.findElement(By.xpath("//span[@class='f-bold busFound']")).getText();
+		Thread.sleep(2000);
+		String str = text.replaceAll("[a-zA-Z]", "");
+		System.out.println("Total busses available:" + str);
+		Thread.sleep(2000);
+		System.out.println("Buses found");
 		
 		File source = driver.getScreenshotAs(OutputType.FILE);
 		
 		File target = new File ("E://Selenium//Day6-Feb28,2021/Redbus.png");
 		
 		FileUtils.copyFile(source, target);
+		
+		//To print unique transport values
+		
+		List<WebElement> findElementsByXPath = driver
+				.findElementsByXPath("//div[@class='clearfix row-one']//div[contains(@class, 'travels')]");
+		List<String> elementText = new ArrayList<String>();
+		for (WebElement webElement : findElementsByXPath) {
+			elementText.add(webElement.getText());
+
+		}
+		Set<String> uniqueBusses = new LinkedHashSet<String>(elementText);
+		List<String> finalList = new ArrayList<String>(uniqueBusses);
+		System.out.println("Travel available eliminating the duplicates:");
+		for (String string : finalList) {
+			System.out.println(i + " " + string);
+			i++;
+		}
 		
 		
 		
